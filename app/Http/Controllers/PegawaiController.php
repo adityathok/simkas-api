412eb00ee38d86f7037989d70aeb96d619e8ba4d
Aditya Thok <aditya0thok@gmail.com>
2024-12-23 23:41:02 +0700

    api pegawai

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Pegawai;
+
+class PegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $Pegawai = Pegawai::paginate(50);
+        $Pegawai->withPath('/pegawai');
+        return response()->json($Pegawai);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\UnitSekolahController;
+use App\Http\Controllers\PegawaiController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -17,6 +18,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResources([
         'users' => UsersController::class,
         'unitsekolah' => UnitSekolahController::class,
+        'pegawai' => PegawaiController::class,
         // 'posts' => PostController::class,
     ]);
 });
