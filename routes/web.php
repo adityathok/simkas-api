973ed7379c2284fb456f66caf7ec9cdb6b10b732
adityathok <aditya0thok@gmail.com>
2024-12-23 13:36:48 +0700

    env

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'Laravel'   => app()->version(),
+        'app_name'  => 'Sistem Administrasi Keuangan Sekolah',
+        'date'      => date("d-m-y H:i:s"),
+        'version'   => '0.0.1',
+    ];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
