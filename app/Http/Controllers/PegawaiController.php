18e530d8b3947983be6030f96d7314d0425e296f
Aditya Thok <aditya0thok@gmail.com>
2024-12-29 22:05:43 +0700

    pegawai akun add

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Pegawai;
+use App\Models\User;
 
 class PegawaiController extends Controller
 {
@@ -25,7 +26,32 @@ class PegawaiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate
+        $request->validate([
+            'nip'           => 'required|min:3',
+            'nama'          => 'required|min:4',
+            'status'        => 'required|min:4',
+            'tempat_lahir'  => 'required|min:4',
+            'tanggal_lahir'  => 'required|min:4',
+            'tanggal_masuk'  => 'required|min:4',
+            'jenis_kelamin'  => 'required|min:4',
+            'nik'           => 'min:10',
+            'email'          => 'min:10',
+        ]);
+
+        $pegawai = new Pegawai();
+        $pegawai->nip = $request->nip;
+        $pegawai->nama = $request->nama;
+        $pegawai->status = $request->status;
+        $pegawai->tempat_lahir = $request->tempat_lahir;
+        $pegawai->tanggal_lahir = $request->tanggal_lahir;
+        $pegawai->tanggal_masuk = $request->tanggal_masuk;
+        $pegawai->jenis_kelamin = $request->jenis_kelamin;
+        $pegawai->nik = $request->nik;
+        $pegawai->email = $request->email;
+        $pegawai->save();
+
+        return response()->json($pegawai);
     }
 
     /**
@@ -48,8 +74,32 @@ class PegawaiController extends Controller
     public function update(Request $request, string $id)
     {
 
+        $request->validate([
+            'nip'       => 'required|min:3',
+            'nama'      => 'required|min:4',
+            'status'    => 'required|min:4',
+            'tempat_lahir'  => 'required|min:4',
+            'tanggal_lahir'  => 'required|min:4',
+            'tanggal_masuk'  => 'required|min:4',
+            'jenis_kelamin'  => 'required|min:4',
+            'nik'  => 'min:10',
+            'email'  => 'min:10',
+        ]);
+
         $pegawai = Pegawai::find($id);
-        $pegawai->update($request->except(['foto']));
+
+        //update
+        $pegawai->update([
+            'nip'   => $request->nip,
+            'nama'  => $request->nama,
+            'status' => $request->status,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'tanggal_masuk' => $request->tanggal_masuk,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'nik' => $request->nik,
+            'email' => $request->email,
+        ]);
 
         //upload gambar
         if ($request['foto'] && $request->file('foto')) {
@@ -96,9 +146,49 @@ class PegawaiController extends Controller
         ]);
     }
 
-    public function akun(string $id)
+    public function akun(Request $request, string $id)
     {
-        $pegawai = Pegawai::find($id);
-        return response()->json($pegawai);
+        $pegawai = Pegawai::select('id', 'nama', 'email', 'user_id')->with(['user'])->findOrFail($id);
+
+        // Validasi input jika diperlukan (untuk POST)
+        if ($request->isMethod('post')) {
+            $request->validate([
+                'name'      => 'required|min:3',
+                'email'     => 'required|min:10',
+                'password'  => ['required', 'confirmed', Rules\Password::defaults()],
+            ]);
+
+            if ($pegawai->user_id == null) {
+
+                // Tambah data ke User
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'type' => 'pegawai',
+                    'password' => Hash::make($request->string('password')),
+                ]);
+                $user_id = $user->id;
+
+                // Tambah data ke Pegawai
+                $pegawai->update([
+                    'user_id' => $user_id
+                ]);
+            } else {
+
+                $user = User::find($pegawai->user_id);
+                //update
+                $user->update([
+                    'password' => Hash::make($request->string('password')),
+                ]);
+            }
+
+            $pegawai = Pegawai::select('id', 'nama', 'email', 'user_id')->with(['user'])->findOrFail($id);
+            return response()->json($pegawai);
+        }
+
+        // Handle GET: Ambil data pegawai
+        if ($request->isMethod('get')) {
+            return response()->json($pegawai);
+        }
     }
 }
