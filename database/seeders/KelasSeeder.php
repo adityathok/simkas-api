e6a9e5cc2d5f9a3316ae1f0b47cab7949410a4ec
Aditya Thok <aditya0thok@gmail.com>
2025-01-26 08:12:41 +0700

    kelas seeder get pegawai

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Kelas;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\UnitSekolah;
+use App\Models\Pegawai;
 
 class KelasSeeder extends Seeder
 {
@@ -22,6 +23,9 @@ class KelasSeeder extends Seeder
             $tingkats = $unitSekolah->tingkat ? json_decode($unitSekolah->tingkat) : [1];
             $rombels = $unitSekolah->rombel ? json_decode($unitSekolah->rombel) : [];
 
+            // Contoh sederhana mendapatkan id guru
+            $pegawai = Pegawai::inRandomOrder()->first();
+
             foreach ($tingkats as $tingkat) {
                 if ($rombels) {
                     foreach ($rombels as $rombel) {
@@ -29,7 +33,8 @@ class KelasSeeder extends Seeder
                             'unit_sekolah_id'   => $unitSekolah->id,
                             'tingkat'           => $tingkat,
                             'tahun_ajaran'      => date('Y', strtotime('-1 year')) . '/' . date('Y'),
-                            'nama'              => $tingkat . ' ' . $rombel
+                            'nama'              => $tingkat . ' ' . $rombel,
+                            'wali_id'           => $pegawai->user_id
                         ]);
                     }
                 } else {
@@ -37,7 +42,8 @@ class KelasSeeder extends Seeder
                         'unit_sekolah_id'   => $unitSekolah->id,
                         'tingkat'           => $tingkat,
                         'tahun_ajaran'      => date('Y', strtotime('-1 year')) . '/' . date('Y'),
-                        'nama'              => $tingkat
+                        'nama'              => $tingkat,
+                        'wali_id'           => $pegawai->user_id
                     ]);
                 }
             }
