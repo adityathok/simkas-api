2e4d2ac1a9e0d24075f8967a8a60a93733386873
Aditya Thok <aditya0thok@gmail.com>
2025-05-10 01:08:40 +0700

    refactory: transaksi table

--- database/migrations/2025_03_22_025235_create_transaksis_table.php
+++ database/migrations/2025_03_22_025235_create_transaksis_table.php
@@ -12,24 +12,27 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('transaksis', function (Blueprint $table) {
-            $table->char('id', 26)->primary();
-            $table->string('nama');
+            $table->id();
             $table->decimal('nominal', 15, 2);
             $table->enum('arus', ['masuk', 'keluar']);
             $table->char('pendapatan_id', 26)->nullable();
             $table->char('pengeluaran_id', 26)->nullable();
             $table->char('rekening_id', 26)->nullable();
-            $table->foreignId('tagihan_id')->constrained()->nullable();
+            $table->char('sumber_rekening_id', 26)->nullable();
             $table->foreignId('user_id')->nullable();
             $table->foreignId('admin_id')->nullable()->constrained('users')->onDelete('set null');
             $table->text('keterangan')->nullable();
+            $table->foreignId('ref_id')->nullable()->constrained('transaksis')->onDelete('set null');
             $table->timestamp('tanggal');
+            $table->string('metode_pembayaran');
+            $table->enum('status', ['pending', 'sukses', 'gagal', 'batal']);
             $table->timestamps();
             $table->softDeletes();
 
             $table->foreign('pendapatan_id')->references('id')->on('akun_pendapatans')->onDelete('set null');
             $table->foreign('pengeluaran_id')->references('id')->on('akun_pengeluarans')->onDelete('set null');
             $table->foreign('rekening_id')->references('id')->on('akun_rekenings')->onDelete('set null');
+            $table->foreign('sumber_rekening_id')->references('id')->on('akun_rekenings')->onDelete('set null');
         });
     }
 
